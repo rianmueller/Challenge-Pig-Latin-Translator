035602be2d1c40a3422f3c930225c4bcca2e4966
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-09-05 17:12:32 -1000

    resolves toEnglish tests (had to remove period from test string)

--- translator.js
+++ translator.js
@@ -3,12 +3,12 @@ class Translator {
     // take a string
     // split the string by spaces
     // process each word individually
-    // put a hyphen at the end,
-    // if the first letter is a consonant
-    // then move first letter to the end after the hyphen
-    // repeat until the first letter is a vowel
-    // then append "ay" to the end
-    // recombine all the words
+    // - put a hyphen on the end of the word
+    // - if the first letter is a consonant
+    //   - then move first letter to the end after the hyphen
+    //   - and repeat until the first letter is a vowel
+    // - then put "ay" on the end
+    // recombine the words into a new string
     let array = string.split(" ");
     let newArray = [];
 
@@ -32,6 +32,35 @@ class Translator {
 
     array.forEach(function(element) {
       element = element + "-";
+      newArray.push(iterator(element));
+    });
+    let newString = newArray.join(" ");
+    return newString;
+  }
+
+  toEnglish(string) {
+    // take a string
+    // split the string by spaces
+    // process each word individually
+    // remove "ay" from the end of each word
+    // take all letters before the hyphen and move to the end
+    // remove the hyphen
+    // recombine the words into a new string
+    let array = string.split(" ");
+    let newArray = [];
+
+    function iterator(element) {
+      if (element.startsWith("-")) {
+        // remove hyphen and return
+        return element.substring(1);
+      }
+      // if element starts with other than hyphen
+      // move the first letter to the end and recurse
+      return iterator(element.substring(1) + element.substring(0, 1));
+    }
+
+    array.forEach(function(element) {
+      element = element.substring(0, element.length - 2);
       newArray.push(iterator(element));
     });
     let newString = newArray.join(" ");
