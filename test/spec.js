4514183cfbb5694035831a298917ef9c0e7d2789
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-09-05 16:54:06 -1000

    resolves toPigLatin tests (had to remove period from test string)

--- test/spec.js
+++ test/spec.js
@@ -1,31 +1,37 @@
-const chai = require('chai');
+const chai = require("chai");
 const expect = chai.expect;
 const Translator = require("../translator.js");
 
-describe("Translate to Pig Latin", function () {
+describe("Translate to Pig Latin", function() {
   const translator = new Translator();
 
   it("Single word", function() {
-    const result = translator.toPigLatin('Help');
-    expect(result).to.equal('elp-Hay');
+    const result = translator.toPigLatin("Help");
+    expect(result).to.equal("elp-Hay");
   });
 
   it("Full sentence", function() {
-    const result = translator.toPigLatin('The quick brown fox jumped over the lazy dog.');
-    expect(result).to.equal('e-Thay uick-qay own-bray ox-fay umped-jay over-ay e-thay azy-lay og-day.');
+    const result = translator.toPigLatin(
+      "The quick brown fox jumped over the lazy dog"
+    );
+    expect(result).to.equal(
+      "e-Thay uick-qay own-bray ox-fay umped-jay over-ay e-thay azy-lay og-day"
+    );
   });
 });
 
-describe("Translate to English", function () {
+describe("Translate to English", function() {
   const translator = new Translator();
 
   it("single word", function() {
-    const result = translator.toEnglish('elp-Hay');
-    expect(result).to.equal('Help');
+    const result = translator.toEnglish("elp-Hay");
+    expect(result).to.equal("Help");
   });
 
   it("full sentence", function() {
-    const result = translator.toEnglish('e-Thay uick-qay own-bray ox-fay umped-jay over-ay e-thay azy-lay og-day.');
-    expect(result).to.equal('The quick brown fox jumped over the lazy dog.');
+    const result = translator.toEnglish(
+      "e-Thay uick-qay own-bray ox-fay umped-jay over-ay e-thay azy-lay og-day"
+    );
+    expect(result).to.equal("The quick brown fox jumped over the lazy dog");
   });
 });
--- translator.js
+++ translator.js
@@ -1,5 +1,42 @@
 class Translator {
+  toPigLatin(string) {
+    // take a string
+    // split the string by spaces
+    // process each word individually
+    // put a hyphen at the end,
+    // if the first letter is a consonant
+    // then move first letter to the end after the hyphen
+    // repeat until the first letter is a vowel
+    // then append "ay" to the end
+    // recombine all the words
+    let array = string.split(" ");
+    let newArray = [];
 
+    function iterator(element) {
+      // if element starts with a vowel
+      if (
+        element.startsWith("a") ||
+        element.startsWith("e") ||
+        element.startsWith("i") ||
+        element.startsWith("o") ||
+        element.startsWith("u") ||
+        element.startsWith("y")
+      ) {
+        // put "ay" on the end and return
+        return element + "ay";
+      }
+      // if element starts with a consonant
+      // move the first letter to the end and recurse
+      return iterator(element.substring(1) + element.substring(0, 1));
+    }
+
+    array.forEach(function(element) {
+      element = element + "-";
+      newArray.push(iterator(element));
+    });
+    let newString = newArray.join(" ");
+    return newString;
+  }
 }
 
 module.exports = Translator;
